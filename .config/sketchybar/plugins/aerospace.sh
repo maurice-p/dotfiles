6538c14186d9972de52edb7b89d3b5cef7dbfb95
maurice-p <maurice.preussner@gmail.com>
2025-03-31 17:31:30 +0200

    Aerospace Sketchybar setup with workspace icon assigned to specific monitor

--- .config/sketchybar/plugins/aerospace.sh
+++ .config/sketchybar/plugins/aerospace.sh
@@ -3,9 +3,14 @@
 # make sure it's executable with:
 # chmod +x ~/.config/sketchybar/plugins/aerospace.sh
 
-
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME background.drawing=on
+  sketchybar --set $NAME background.color=0xFFecdbb2 label.shadow.drawing=on icon.shadow.drawing=on background.border_width=1
+  sketchybar --set $NAME icon.color=0xFF3c3836 label.color=0xFF3c3836 
+  # icon.shadow.color=0xFFecdbb2 label.shadow.color=0xFFecdbb2
+
+
 else
-    sketchybar --set $NAME background.drawing=off
+  sketchybar --set $NAME background.color=0xFF3c3836 label.shadow.drawing=off icon.shadow.drawing=off background.border_width=0
+  sketchybar --set $NAME icon.color=0xFFecdbb2 label.color=0xFFecdbb2  
+  # icon.shadow.color=0xFF3c3836 label.shadow.color=0xFF3c3836
 fi
